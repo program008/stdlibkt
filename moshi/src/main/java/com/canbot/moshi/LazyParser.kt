7c9059d7e7b011bf56b6d185ee29c1a092d20814
liutao <tao.liu@uurobot.cn>
2018-12-22 11:27:59 +0800

    correlation

--- moshi/src/main/java/com/canbot/moshi/LazyParser.kt
+++ moshi/src/main/java/com/canbot/moshi/LazyParser.kt
@@ -3,13 +3,13 @@ package com.canbot.moshi
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.Moshi
-import kotlin.coroutines.experimental.buildSequence
+//import kotlin.coroutines.experimental.buildSequence
 
 class LazyParser(moshi: Moshi) {
     private val personAdapter: JsonAdapter<Person> = moshi.adapter(Person::class.java)
 
     fun parse(reader: JsonReader): Sequence<Person> {
-        return buildSequence {
+        return sequence {
             reader.readArray {
                 yield(personAdapter.fromJson(reader)!!)
             }
