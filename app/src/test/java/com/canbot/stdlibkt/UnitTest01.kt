17c469e21cade6cb121272d248fafefd5558e404
liutao <tao.liu@uurobot.cn>
2018-12-25 14:43:32 +0800

    let with also apply run

--- app/src/test/java/com/canbot/stdlibkt/UnitTest01.kt
+++ app/src/test/java/com/canbot/stdlibkt/UnitTest01.kt
@@ -11,7 +11,13 @@ import kotlin.contracts.contract
 class UnitTest01 {
         @Test
         fun fun1() {
+                val numbers = listOf(1, -2, 3, -4, 5, -6)            // 1
 
+                val totalCount = numbers.count()                     // 2
+                val evenCount = numbers.count { it % 2 == 0 }        // 3
+
+                println("Total number of elements: $totalCount")
+                println("Number of even elements: $evenCount")
         }
 
         fun foo(string: String?) {
@@ -36,4 +42,25 @@ class UnitTest01 {
                 contract { returns(false) implies (this@isNullOrEmpty != null) }
                 return this == null || isEmpty()
         }*/
+
+        @Test
+        fun test02(){
+                data class Person(val name: String, val city: String, val phone: String) // 1
+
+                val people = listOf(                                                     // 2
+                        Person("John", "Boston", "+1-888-123456"),
+                        Person("Sarah", "Munich", "+49-777-789123"),
+                        Person("Svyatoslav", "Saint-Petersburg", "+7-999-456789"),
+                        Person("Vasilisa", "Saint-Petersburg", "+7-999-123456"))
+
+                val phoneBook = people.associateBy { it.phone }                          // 3
+                val cityBook = people.associateBy(Person::phone, Person::city)           // 4
+                val peopleCities = people.groupBy(Person::city, Person::name)            // 5
+
+
+                println("People: $people")
+                println("Phone book: $phoneBook")
+                println("City book: $cityBook")
+                println("People living in each city: $peopleCities")
+        }
 }
